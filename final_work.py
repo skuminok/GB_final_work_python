3a37d62a7aae6af3c5b717788886f957f129fb65
skuminok <skuminok@gmail.com>
2024-07-25 20:48:58 +0300

    GB_final_work_python

---
+++ final_work.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import random
+
+# Генерируем исходные данные
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Преобразуем в one-hot вид
+one_hot_data = pd.DataFrame()
+for category in data['whoAmI'].unique():
+    one_hot_data[category] = (data['whoAmI'] == category).astype(int)
+
+# Печатаем первые несколько строк
+print(one_hot_data.head())
